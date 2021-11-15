9aeeec9e9cb38ad64e7a650a86a6fecb7a0454ed
Jack Smith <jack.je.smith@gmail.com>
2021-11-15 17:26:36 +0000

    Use file_on_disk explicitly

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import Linter
 
 
 class JSONSchemaLint(Linter):
-    cmd = 'jsonschema-lint ${file}'
+    cmd = 'jsonschema-lint ${file_on_disk}'
     name = 'jsonschema-lint'
     regex = r'(?P<filename>.*):(?P<line>[1-9][0-9]*):(?P<col>[1-9][0-9]*):(?P<end_line>[1-9][0-9]*):(?P<end_col>[1-9][0-9]*): (?P<message>.*)'
     tempfile_suffix = "-"
